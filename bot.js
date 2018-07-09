8518934c73b4f594cdc2874520c3178b9cce6549
MajimaGorou <40967673+MajimaGorou@users.noreply.github.com>
2018-07-08 23:24:13 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -13,7 +13,20 @@ const prefix ="n!"
 bot.on('message', message => {
   if(message.author.bot)return;
 
-
+        if(message.member.roles.find("name", "Officer")){
+        var person="boss";
+        }     
+        else if(message.member.roles.find("name", "she/her")){
+        var person="madam";
+        } 
+        else if(message.member.roles.find("name", "he/him")){
+        var person="sir";
+        }
+        else {
+        var person="captain";
+        }
+  
+  
 
 if(message.content.toLowerCase().includes( ['bepsi'] ) || message.content.toLowerCase().includes( ['pepsi'] ) ){
        message.react(":bepsi:403237730919645224");
@@ -23,10 +36,11 @@ if(message.content.toLowerCase().includes( ['bepsi'] ) || message.content.toLowe
 if(message.content.toLowerCase().includes( ['this is so'] ) && message.content.toLowerCase().includes( ['nishida play'] ) ){
 
     if(message.content.toLowerCase().includes( ['24'] )){
-    message.channel.sendFile("https://a.tumblr.com/tumblr_pbkv6cXucB1xv33e9o1.mp3","24 Hour Cinderella.mp3" )
+        message.channel.send("Here you go, " + person +"!");    
+        message.channel.sendFile("https://a.tumblr.com/tumblr_pbkv6cXucB1xv33e9o1.mp3","24 Hour Cinderella.mp3" )
     } 
 
-}  
+}
   
   
 if (message.content.startsWith(prefix + "hey nishida"))  {
